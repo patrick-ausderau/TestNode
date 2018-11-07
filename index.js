d1e2b9486210cebb766af8daaa44e3b59db77567
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2018-11-07 14:14:47 +0200

    at

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Modified, do you got the point?');
   console.log('Server GET called');  
-})
+});
+
+app.get('/test', (req, res) => {
+  res.send('Hello Test');
+  console.log('Server GET /test called');  
+});
+
  
 app.listen(3000);
